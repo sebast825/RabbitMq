04eb27429ced024faf6eefa2a27306066267bc27
sebast825 <sebamolina825@gmail.com>
2025-10-24 17:08:26 -0300

    rabbit config

--- BasicProducer/Program.cs
+++ BasicProducer/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Basic Producer iniciado!");
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+// En versiones recientes se usa ConnectAsync o CreateConnection de esta forma:
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: "",
+                     routingKey: "hello",
+                     body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Presiona [enter] para salir.");
+Console.ReadLine();
